9febef94e594feadc889aa2ce3fb58a44c534e24
rogeryoungh <rogeryoungh@foxmail.com>
2022-06-01 20:45:57 +0800

    udpate: add lint & format

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:import/typescript',
+    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+};
