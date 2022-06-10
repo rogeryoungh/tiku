4759d0ce1c7301532501526e268665a7e8a174f9
rogeryoungh <rogeryoungh@foxmail.com>
2022-06-10 22:13:50 +0800

    update: add home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,109 @@
 <script setup lang="ts">
-import { NButton } from 'naive-ui';
+import {
+  NButton,
+  NH1,
+  NP,
+  NList,
+  NListItem,
+  NCard,
+  useMessage,
+  NH3,
+  NH2
+} from 'naive-ui';
+import { onMounted, ref } from 'vue';
+import { TomlReader } from '@sgarciac/bombadil';
+import { useRoute } from 'vue-router';
+
+const reader = new TomlReader();
+const route = useRoute();
+const message = useMessage();
+
+const url = String(route.query.url || 'http://10.42.0.1:4321/tmp.toml');
+
+const dataRef = ref<any>({});
+
+const parserData = (input: any) => {
+  reader.readToml(input);
+  let data = reader.result;
+  console.log(data);
+  dataRef.value.name = data.name;
+  dataRef.value.url = url;
+  let problems = [];
+  for (let p of data.problems) {
+    problems.push({
+      content: p.content,
+      answer: p.answer,
+      analysis: p.analysis,
+      show: false
+    });
+  }
+  dataRef.value.problems = problems;
+  return 0;
+};
+
+const fetchData = (url: string) => {
+  console.log(url);
+  fetch(url)
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      parserData(data);
+    })
+    .catch(e => {
+      console.error(e);
+      message.error('加载失败');
+    });
+};
+
+let isShowAll = ref(false);
+
+const showAll = () => {
+  isShowAll.value = !isShowAll.value;
+  for (let p of dataRef.value.problems) {
+    p.show = isShowAll.value;
+  }
+};
+
+onMounted(() => {
+  fetchData(url);
+});
 </script>
 
 <template>
-  <n-button>hhh</n-button>
+  <div id="home-container">
+    <div v-if="dataRef != null">
+      <n-h1> {{ dataRef.name }} </n-h1>
+      <n-p> URL：{{ dataRef.url }}</n-p>
+      <n-button type="primary" @click="showAll">
+        {{ isShowAll ? '全部展开' : '全部闭合' }}
+      </n-button>
+      <n-list v-if="dataRef.problems != null">
+        <n-list-item v-for="p of dataRef.problems" :key="p.id">
+          <n-card>
+            <n-h2> 题目 </n-h2>
+            <n-p> {{ p.content }} </n-p>
+            <n-button secondary type="primary" @click="p.show = !p.show">
+              查看解析
+            </n-button>
+            <n-p v-if="p.show">
+              <n-h3> 答案 </n-h3>
+              <n-p> {{ p.content }} </n-p>
+            </n-p>
+            <n-p v-if="p.show && p.analysis !== ''">
+              <n-h3> 分析 </n-h3>
+              <n-p> {{ p.analysis }} </n-p>
+            </n-p>
+          </n-card>
+        </n-list-item>
+      </n-list>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style lang="less" scoped>
+#home-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 40px;
+}
+</style>
